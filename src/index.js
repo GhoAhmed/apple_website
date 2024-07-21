102e2c47b2e9c37bd8a86a9954a4ebbc5d5a9bb5
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2024-07-22 00:59:01 +0200

    config : constants , utils and public folders

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-  </h1>
+  <main className='bg-black'>
+    Navbar
+    Hero
+  </main>
 );
