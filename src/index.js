d163cd8541b6b5b4512838f5535d0acc4a460152
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2024-07-22 02:10:42 +0200

    assets config and Navbar implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <main className='bg-black'>
-    Navbar
-    Hero
+  <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <Highlights />
   </main>
 );
