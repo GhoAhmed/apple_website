4377d21dc1ff3c80620bb9a6483aefe3eea53fe6
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2024-07-23 02:11:51 +0200

    Footer & cleaning up

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Highlights from './components/Highlights';
 import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,6 +19,7 @@ root.render(
     <Model />
     <Features />
     <HowItWorks />
+    <Footer />
   </main>
 );
 
